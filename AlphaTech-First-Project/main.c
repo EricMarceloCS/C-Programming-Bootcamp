7a90f6d9c2cbbe9c306a6f220ac735051960b1c8
eric <ericmarcelo455@gmail.com>
2023-05-02 15:58:23 -0700

    Print Exercises
    * print "Hello Alpha Tech!"
    * print Name, Age, and Gender
    * Print Isoceles Triangle

--- AlphaTech-First-Project/main.c
+++ AlphaTech-First-Project/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+
+    printf("Hello Alpha Tech!\n");
+    printf("Name: Brad Pitt\n");
+    printf("Age: 55\n");
+    printf("Gender: Male\n");
+    printf("Name: Brad Pitt\nAge: 55\nGender: Male\n");
+
+    printf("  *  \n");
+    printf(" *** \n");
+    printf("*****\n");
+
+    printf("  *  \n *** \n*****\n");
+
     return 0;
 }
